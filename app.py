6bce4acc59c80f11287112ea3681119772ce7b2a
Rolfrider-Mac <ravkwiatkowski@gmail.com>
2020-05-01 09:50:57 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,4 @@
+from read.read import get_acceptors, get_donors
+
+print(get_acceptors())
+print(get_donors())
---
+++ read/read.py
@@ -0,0 +1,23 @@
+
+data_folder = "data/"
+donor_file = "spliceDTrainKIS.dat"
+acceptor_file = "spliceATrainKIS.dat"
+
+
+def parse_examples(lines):
+    size = len(lines) - 1
+    return [(lines[i].strip('\n'), lines[i+1].strip('\n')) for i in range(0, size, 2)]
+
+
+def read_file(file_name):
+    with open(data_folder + donor_file) as file:
+        lines = file.readlines()
+        return parse_examples(lines[1:])
+
+
+def get_donors():
+    return read_file(donor_file)
+
+
+def get_acceptors():
+    return read_file(acceptor_file)
