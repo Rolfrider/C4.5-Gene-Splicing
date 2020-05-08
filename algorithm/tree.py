83a2826cbeef78ae9dd9c73c67ea170c5de127cc
Rolfrider-Mac <ravkwiatkowski@gmail.com>
2020-05-08 09:48:16 +0200

    Add c4.5 cutting algo ✂️

--- algorithm/tree.py
+++ algorithm/tree.py
@@ -2,11 +2,16 @@ from algorithm.data_struct import Example
 from algorithm.entropy import *
 from abc import ABC, abstractmethod
 from queue import PriorityQueue
+from math import sqrt
+
 
 def build_tree(training_data: [Example]):
     attr_indecies = [i for i in range(0, len(training_data[0].attributes))]
     tree = id3(attr_indecies, training_data)
+    print("Before c4.5:")
+    print(tree)
     return c45(tree)
+
 
 def id3(attr_indecies: [int], examples: [Example], level: int = 0):
     if all(example.positive for example in examples):
@@ -27,15 +32,45 @@ def id3(attr_indecies: [int], examples: [Example], level: int = 0):
 
     next_level = level + 1
 
-    children = { val : id3(attr_indecies, exps, next_level) for val, exps in value_examples.items() }
+    children = {val: id3(attr_indecies, exps, next_level)
+                for val, exps in value_examples.items()}
 
     return Branch(level, examples, best_attr, children)
 
+
 def c45(tree):
-    return get_branches(tree)
+    branches = get_branches(tree)
+    for branch in sorted(branches, key=lambda x: x.level, reverse=True):
+        branch_error = error(branch, branch.train_examples)
+        positives, negatives = count_classes(branch.train_examples)
+        leaf = Leaf(branch.level, branch.train_examples, True) if positives > negatives else Leaf(
+            branch.level, branch.train_examples, False)
+        leaf_error = error(leaf, leaf.train_examples)
+        if branch_error >= leaf_error:
+            tree.replace_branch(branch, leaf)
+    return tree
 
 
-def get_branches(node, branches = []):
+def error(tree, examples):
+    error_on_training = training_set_error(tree, examples)
+    return estimated_error(error_on_training, len(examples))
+
+
+def estimated_error(training_error, examples_count):
+    return training_error + (sqrt(training_error + training_error**2) / examples_count)
+
+
+def training_set_error(tree, examples):
+    evaluated_well = 0
+    for example in examples:
+        value = tree.determine(example.attributes)
+        if value == example.positive:
+            evaluated_well += 1
+
+    return evaluated_well/len(examples)
+
+
+def get_branches(node, branches=[]):
     if isinstance(node, Leaf):
         return branches
     else:
@@ -51,10 +86,12 @@ def get_branches(node, branches = []):
 def inf_gain(attr_index: int, examples: [Example]):
     return entropy(examples) - inf(attr_index, examples)
 
+
 def inf(attr_index: int, examples: [Example]):
     value_examples = split(attr_index, examples)
     examples_count = len(examples)
-    return sum( single_inf(attr_examples, examples_count) for attr_examples in value_examples.values())
+    return sum(single_inf(attr_examples, examples_count) for attr_examples in value_examples.values())
+
 
 def split(attr_index: int, examples: [Example]):
     value_examples = dict()
@@ -62,10 +99,11 @@ def split(attr_index: int, examples: [Example]):
         key = exp.attributes[attr_index]
         value_examples[key] = value_examples.get(key, []) + [exp]
     return value_examples
-    
+
+
 def single_inf(attr_examples: [Example], examples_count: int):
     return (len(attr_examples)/examples_count) * entropy(attr_examples)
-        
+
 
 class Node(ABC):
 
@@ -80,6 +118,7 @@ class Node(ABC):
     def __str__(self):
         return "Level: " + str(self.level)
 
+
 class Branch(Node):
     def __init__(self, level, train_examples: [Example], spliting_attr: int, children):
         super().__init__(level, train_examples)
@@ -92,12 +131,22 @@ class Branch(Node):
     def children_str(self):
         string = ""
         for val, node in self.children.items():
-             string += "\nFor value: " + str(val) + " have " + str(node)
+            string += "\nFor value: " + str(val) + " have " + str(node)
         return string
 
     def determine(self, attribiutes):
         key = attribiutes[self.spliting_attr]
         return self.children[key].determine(attribiutes)
+
+    def replace_branch(self, branch, node):
+        if all(isinstance(child, Leaf) for child in self.children.values()):
+            return
+        for key, child in self.children.items():
+            if child == branch:
+                self.children[key] = node
+                return
+            elif isinstance(child, Branch):
+                child.replace_branch(branch, node)
 
 
 class Leaf(Node):
--- app.py
+++ app.py
@@ -4,18 +4,22 @@ from algorithm.tree import build_tree
 
 # print(get_acceptors())
 # print(get_donors())
-
-examples = [
-    Example("EWA", '1'),
-    Example("ADA", '0'),
-    Example("OLA", '0'),
-    Example("IGA", '0'),
-    Example("ALA", '1'),
-    Example("IZA", '0'),
-    Example("IDA", '0')
-]
+donors = get_donors()
+examples = list(map(lambda x: Example(x[1], x[0]), donors))
+# examples = [
+#     Example("EWAASDWAS", '1'),
+#     Example("ADAKNBJHK", '1'),
+#     Example("OJNKJNKLA", '0'),
+#     Example("IGBJKNJKA", '0'),
+#     Example("ALKJKKJFA", '1'),
+#     Example("IZBCVXDFA", '0'),
+#     Example("IDMOKMKJA", '0'),
+#     Example("ALMOKMKJA", '0'),
+#     Example("IDMOKPKJA", '1')
+# ]
 tree = build_tree(examples)
 # result = tree.determine("EWA")
-# print(result)
-for branch in tree:
-    print(branch)
+print("After c4.5")
+print(tree)
+# for branch in sorted(tree, key=lambda x: x.level, reverse=True):
+#     print(branch)
