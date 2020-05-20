e2901c72aaec3b192c08d0e9c71ad34124d9923c
Damazy31 <Damazy_31@interia.pl>
2020-05-20 19:17:35 +0200

    crossing validation update

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from read.read import get_acceptors, get_donors
 from algorithm.data_struct import Example
 from algorithm.tree import build_tree
-from tests.crossing_validation import test_crossing_validation
+from tests.crossing_validation import test_crossing_validation, crossing_validation
 
 # print(get_acceptors())
 # print(get_donors())
@@ -11,6 +11,7 @@ examples = list(map(lambda x: Example(x[1], x[0]), acteptors))
 # tree = build_tree(examples)
 # print("After c4.5")
 # print(tree)
-test_crossing_validation(examples)
+print(crossing_validation(examples, 0.5))
+#test_crossing_validation(examples)
 # for branch in sorted(tree, key=lambda x: x.level, reverse=True):
 #     print(branch)
--- tests/crossing_validation.py
+++ tests/crossing_validation.py
@@ -3,51 +3,46 @@ from algorithm.tree import build_tree
 import json
 
 def test_crossing_validation(examples: [Example]):
-    iteration = 3
-    set_dividers = [0.2, 0.25, 0.5]
+    set_dividers = [0.05, 0.1, 0.2, 0.25, 0.3, 0.4, 0.5]
     results = []
     for divider in set_dividers:
-        result = 0
-        for i in range(0, iteration):
-            result = result + crossing_validation(examples, divider)
-        results.append(result)
+        results.append(crossing_validation(examples, divider))
     file_name = "results/crossing_validation.json"
     with open(file_name, "w") as file:
         json.dump(results, file)
     
 
-def crossing_validation(examples: [Example], testing: float):
+def crossing_validation(examples: [Example], subset_size: float):
     size = len(examples)
-    testing_size = (int)(size*testing)
+    testing_size = (int)(size*subset_size)
     remaining_examples = size
     accuracy = 0
     iteration = 0
 
     while remaining_examples > 0:
-        if remaining_examples > testing_size:
+        if remaining_examples >= testing_size:
             testing_set = examples[remaining_examples-testing_size:remaining_examples]
-            training_set = examples[:remaining_examples-testing_size]
-            training_set.extend(examples[remaining_examples:])
+            training_set = examples[:remaining_examples-testing_size] + examples[remaining_examples:]
             remaining_examples = remaining_examples - testing_size
         else:
-            testing_set = examples[0:remaining_examples]
+            testing_set = examples[:remaining_examples]
             training_set = examples[remaining_examples:]
             remaining_examples = 0
 
-        #print(len(testing_set))
-        #print(len(training_set))
+        print(len(testing_set))
+        print(len(training_set))
         tree = build_tree(training_set)
         results = [tree.determine(x.attributes) for x in testing_set]
         
-        accuracy = accuracy + count_accuracy(results)
+        accuracy = accuracy + count_accuracy(results, testing_set)
         iteration = iteration + 1
     
     return accuracy/iteration
 
-def count_accuracy(results: [bool]):
-    positive = 0
-    for example in results:
-        if example == True:
-            positive = positive + 1
-    #print(positive/len(results))
-    return positive/len(results)
+def count_accuracy(results: [bool], testing_set: Example):
+    matching = 0
+    for i in range(0, len(results)):
+        if results[i] == testing_set[i].positive:
+            matching = matching + 1
+    print(matching/len(results))
+    return matching/len(results)
