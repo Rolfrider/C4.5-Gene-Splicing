5c99c932c3f6e3e85e91aabae14f71dd8401e12a
Damazy31 <Damazy_31@interia.pl>
2020-05-23 17:27:30 +0200

    code cleaning

--- app.py
+++ app.py
@@ -3,16 +3,10 @@ from algorithm.data_struct import Example
 from algorithm.tree import build_tree
 from experiments.crossing_validation import test_crossing_validation, crossing_validation
 
-# print(get_acceptors())
-# print(get_donors())
 donors = get_donors()
 acteptors = get_acceptors()
 donors_examples = list(map(lambda x: Example(x[1], x[0]), donors))
 acteptors_examples = list(map(lambda x: Example(x[1], x[0]), acteptors))
-# tree = build_tree(examples)
-# print("After c4.5")
-# print(tree)
+
 #print(crossing_validation(donors_examples, 5, build_tree))
 test_crossing_validation(acteptors_examples, donors_examples)
-# for branch in sorted(tree, key=lambda x: x.level, reverse=True):
-#     print(branch)
